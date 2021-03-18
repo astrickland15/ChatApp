585d1c94ac3bca58357c8fb8383835835bdc3014
astrickland15 <astrickland15@hotmail.com>
2021-03-18 19:04:59 -0400

    Latest File

--- application.py
+++ application.py
@@ -6,10 +6,7 @@ from flask_socketio import SocketIO, send
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "a886e95cfe3b0c51f4f72abf1c1d4c2b"
 socketio = SocketIO(app)
-<<<<<<< HEAD
 connectedUsers = []
-=======
->>>>>>> be60a6fcec755b8a83639492fb5a799946e34cf8
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
@@ -17,7 +14,6 @@ def index():
         return render_template("index.html")
     else:
         user = request.form.get("chatname")
-<<<<<<< HEAD
         if (user not in connectedUsers):
             connectedUsers.append(user)
         return render_template("chat.html",  user = user, connectedUsers = connectedUsers)
@@ -27,9 +23,6 @@ def chat(msg):
     print("Message: " + msg)
     add_to_text_file(msg)
     send(msg, broadcast = True)
-      
-=======
-        return render_template("chat.html",  user = user)
   
 @socketio.on("message")
 def chat(msg):
@@ -38,7 +31,6 @@ def chat(msg):
     send(msg, broadcast = True) """
     
 
->>>>>>> be60a6fcec755b8a83639492fb5a799946e34cf8
 def add_to_text_file(word):
     f = open("c:\\test\chatHistory.txt", "a")
     f.write(word + "\n")
